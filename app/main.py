0713597ed23b1daa76f4ff03468d1e050adc1088
yet92 <zyzy92187@gmail.com>
2024-12-23 20:50:33 +0200

    implement Person class and create_person_list method

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people: dict[str, "Person"] = {}
+
+    def __init__(self, name: str, age: str) -> None:
+        self.name = name
+        self.age = age
+
+        self.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list:
+    persons = [Person(human["name"], human["age"]) for human in people]
+
+    for human in people:
+        if human.get("wife"):
+            Person.people[human["name"]].wife = Person.people[human["wife"]]
+        elif human.get("husband"):
+            Person.people[human["name"]].husband = Person.people[
+                human["husband"]]
+
+    return persons
